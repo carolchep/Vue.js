e017f6f0636806ea9d9c7162acefe4c8fe7a8292
carolchep <carolchepsam@gmail.com>
2022-04-13 15:30:10 +0300

    event listeners

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import hello from '../views/hello.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/hello',
+      name: 'hello',
+      component: hello
     },
     {
       path: '/about',
