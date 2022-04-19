25c606cf18f25d958fbf1922e37d68700d0c2e3c
carolchep <carolchepsam@gmail.com>
2022-04-19 09:28:09 +0300

    counter app

---
+++ src/components/Practs/counter.vue
@@ -0,0 +1,18 @@
+<!--
+https://eugenkiss.github.io/7guis/tasks/#counter
+-->
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<template>
+  {{ count }}
+  <button @click="count++">Count</button>
+</template>
