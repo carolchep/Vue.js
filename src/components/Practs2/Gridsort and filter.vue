45effd51be1ef45303a2152fd9f109240e99da28
carolchep <carolchepsam@gmail.com>
2022-05-11 08:44:26 +0300

    creating a reusable grid component and using it with external data

---
+++ src/components/Practs2/Gridsort and filter.vue
@@ -0,0 +1,23 @@
+<!--
+An example of creating a reusable grid component and using it with external data.
+-->
+
+<script>
+import DemoGrid from './Grid.vue'
+
+export default {
+  components: {
+    DemoGrid
+  },
+  data: () => ({
+    searchQuery: '',
+    gridColumns: ['name', 'power'],
+    gridData: [
+      { name: 'Chuck Norris', power: Infinity },
+      { name: 'Bruce Lee', power: 9000 },
+      { name: 'Jackie Chan', power: 7000 },
+      { name: 'Jet Li', power: 8000 }
+    ]
+  })
+}
+</script>
