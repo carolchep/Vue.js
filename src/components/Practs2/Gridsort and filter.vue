889e6df7f0706091406e745aa348bb24cbe5511a
carolchep <carolchepsam@gmail.com>
2022-05-11 08:47:34 +0300

    grid with sort and filter

--- src/components/Practs2/Gridsort and filter.vue
+++ src/components/Practs2/Gridsort and filter.vue
@@ -21,3 +21,13 @@ export default {
   })
 }
 </script>
+<template>
+  <form id="search">
+    Search <input name="query" v-model="searchQuery">
+  </form>
+  <DemoGrid
+      :data="gridData"
+      :columns="gridColumns"
+      :filter-key="searchQuery">
+  </DemoGrid>
+</template>
