3b777c7664b4411fc3a1e958652878510ca0b9bd
carolchep <carolchepsam@gmail.com>
2022-05-11 08:39:54 +0300

    fetching data

--- src/components/Practs2/Fetchingdata.vue
+++ src/components/Practs2/Fetchingdata.vue
@@ -38,3 +38,41 @@ export default {
   }
 }
 </script>
+
+<template>
+  <h1>Latest Vue Core Commits</h1>
+  <template v-for="branch in branches">
+    <input type="radio"
+           :id="branch"
+           :value="branch"
+           name="branch"
+           v-model="currentBranch">
+    <label :for="branch">{{ branch }}</label>
+  </template>
+  <p>vuejs/vue@{{ currentBranch }}</p>
+  <ul>
+    <li v-for="{ html_url, sha, author, commit } in commits">
+      <a :href="html_url" target="_blank" class="commit">{{ sha.slice(0, 7) }}</a>
+      - <span class="message">{{ truncate(commit.message) }}</span><br>
+      by <span class="author">
+        <a :href="author.html_url" target="_blank">{{ commit.author.name }}</a>
+      </span>
+      at <span class="date">{{ formatDate(commit.author.date) }}</span>
+    </li>
+  </ul>
+</template>
+
+<style>
+a {
+  text-decoration: none;
+  color: #42b883;
+}
+li {
+  line-height: 1.5em;
+  margin-bottom: 20px;
+}
+.author,
+.date {
+  font-weight: bold;
+}
+</style>
