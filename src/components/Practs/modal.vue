4a54f873021632af5e0cdb593021deba881f474d
carolchep <carolchepsam@gmail.com>
2022-05-12 16:41:48 +0300

    Modal component with customizable slots and CSS transitions.

---
+++ src/components/Practs/modal.vue
@@ -0,0 +1,18 @@
+<!--
+Modal component with customizable slots and CSS transitions.
+-->
+
+<script>
+import Modal from './Modal.vue'
+
+export default {
+  components: {
+    Modal
+  },
+  data() {
+    return {
+      showModal: false
+    }
+  }
+}
+</script>
